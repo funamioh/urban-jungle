a28b126044b19a4ba8fc5a6dffd4cf6c17a6b456
Miho Funayama <funamiho1011@gmail.com>
2024-06-06 22:03:28 +0900

    added scraping functionality

--- lib/json_api.rb
+++ lib/json_api.rb
@@ -1,11 +1,28 @@
 require "open-uri"
 require "nokogiri"
 
-url = "https://www.goodhousekeeping.com/home/gardening/advice/g1285/hard-to-kill-plants/"
+module JsonApi
+  class Scraper
+    def initialize(url)
+      @url = url
+    end
 
-html_file = URI.open(url).read
-html_doc = Nokogiri::HTML.parse(html_file)
+    def scrape
+      url = "https://www.goodhousekeeping.com/home/gardening/advice/g1285/hard-to-kill-plants/"
 
-doc.search(".css-prx06t e1bddzxd0").each do |element|
-  puts element.text.strip
+      html_file = URI.open(url).read
+      html_doc = Nokogiri::HTML.parse(html_file)
+      plants = []
+
+      html_doc.search("h2").each do |element|
+        name = element.text.strip
+
+        plants << {
+          name: name
+        }
+      end
+
+      plants.to_json
+    end
+  end
 end
